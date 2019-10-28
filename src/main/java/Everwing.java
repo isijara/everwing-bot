2b4e4ac14498813256fac95ed97b3879589228b8
Ramon Isijara <ramon.isijara@oracle.com>
2019-10-28 10:06:21 -0700

    Printing the coordinates that are being used to perform the clicks

--- src/main/java/Everwing.java
+++ src/main/java/Everwing.java
@@ -20,7 +20,8 @@ public class Everwing {
   }
 
   public void start() throws Exception {
-    this.robot.mouseMove(663, 684);
+    //this.robot.mouseMove(663, 684);
+    this.robot.mouseMove(465, 588);
     this.makeClick();
     Thread.sleep(500);
   }
@@ -38,20 +39,25 @@ public class Everwing {
   }
 
   public void restart() throws Exception {
-    this.robot.mouseMove(625, 751);
+    //this.robot.mouseMove(625, 751);
+    this.robot.mouseMove(396, 666);
+    Thread.sleep(125);
     this.makeClick();
     Thread.sleep(500);
   }
 
   private boolean hasGameFinished() {
-    String color = this.robot.getPixelColor(606, 756).toString();
-    return color.equals("java.awt.Color[r=251,g=255,b=255]");
+    //String color = this.robot.getPixelColor(606, 756).toString();
+    //this.robot.mouseMove(396, 666);
+    String color = this.robot.getPixelColor(396, 666).toString();
+    //System.out.println("The color is " + color);
+    return color.equals("java.awt.Color[r=0,g=160,b=232]");
   }
 
   public void printMouseInfo() {
     Double x = MouseInfo.getPointerInfo().getLocation().getX();
     Double y = MouseInfo.getPointerInfo().getLocation().getY();
-
+    System.out.println("Coordinates: (" + x + "," + y + ")");
     System.out.println(this.robot.getPixelColor( x.intValue(), y.intValue()).toString());
 
   }
@@ -62,8 +68,8 @@ public class Everwing {
 
 
     try {
-      //Thread.sleep(3500);
-      //everwing.printMouseInfo();
+//      Thread.sleep(2000);
+//      everwing.printMouseInfo();
       Thread.sleep(2000);
       for(int i = 0; i < 100000; i ++) {
         System.out.println("Iteration " + (i+1));
